e194ca2b3b5319ca1691b992b4a15dfaa95b0b7b
Jose Cano <cano.5@wright.edu>
2023-09-21 20:27:57 -0400

    Adding id to html scores and removing unused funcs

--- gameScripts.js
+++ gameScripts.js
@@ -1,6 +1,7 @@
 /************  GAME VARIABLES AND FUNCTIONS ***********************************/
 /**************************************************************************** */
-
+let thePlayerPointsGlobal = 5;
+let theAiPointsGlobal = 10;
 const WIN = 1;
 const DRAW = 0;
 const LOSE = -1;
@@ -23,19 +24,6 @@ function convertPlayerSelectionToIndex(playerSelection) {
     return CHOICES.indexOf(playerSelection);
 }
 
-function promptPlayerChoice() {
-    let playerSelection = null;
-
-    while(true){
-        playerSelection = prompt("ROCK, PAPER, OR SCISSORS?");
-        //playerSelection = playerSelection.toUpperCase();
-        if (playerSelection !== null && CHOICES.includes(playerSelection)) {
-            break;
-        }
-    }
-    return CHOICES.indexOf(playerSelection);
-}
-
 
 function playRound(playerSelection, computerIndex) {
     let playerIndex = convertPlayerSelectionToIndex(playerSelection);
@@ -44,49 +32,7 @@ function playRound(playerSelection, computerIndex) {
     return WIN_LOSE_CHART[playerIndex][computerIndex];
 }
 
-function game() {
-    let remainingRounds = 5;
-    let playerPoints = 0;
-    let computerPoints = 0;
-    let playerChoice;
-    let computerChoice;
-    let result;
-    let message;
 
-    while(remainingRounds > 0) {
-
-        playerChoice = promptPlayerChoice();
-        computerChoice = generateComputerChoice();
-        result = playRound(playerChoice, computerChoice);
-
-        switch(result) {
-            case WIN:
-                playerPoints++;
-                console.log("You win");
-                break;
-            case LOSE:
-                computerPoints++;
-                console.log("You lose");
-                break;
-            default:
-                console.log("Draw");
-                // no points incremented in a draw
-        }
-        // decrement remaining rounds
-        //remainingRounds--; 
-    }
-    
-    if (playerPoints > computerPoints){
-        message = "You win!";
-    } else if (playerPoints === computerPoints) {
-        message = "It's a draw!";
-    } else {
-        message = "You lose!";
-    }
-
-    // display the final results
-    window.alert(message + "\nYou: " + playerPoints + " || AI: " + computerPoints);
-}
 
 /**************************************************************************** */
 //                      EVENT FUNCTIONS
@@ -105,3 +51,10 @@ for (let i = 0; i < btns.length; i++) {
 
     });
 }
+
+
+
+
+
+// Testing DOM Changing methods
+document.getElementById("aiPoints").innerHTML = theAiPointsGlobal;
